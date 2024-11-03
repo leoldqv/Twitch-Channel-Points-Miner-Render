4ff35ad4ada17eeb116a5f443e2db114d1fc7bbf
Leo Lundqvist <163914364+leoldqv@users.noreply.github.com>
2024-11-03 21:44:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ twitch_miner = TwitchChannelPointsMiner(
         ),
         telegram=Telegram(                                                          # You can omit or set to None if you don't want to receive updates on Telegram
             chat_id=123456789,                                                      # Chat ID to send messages @getmyid_bot
-            token="123456789:shfuihreuifheuifhiu34578347",                          # Telegram API token @BotFather
+            token="",                                                               # Telegram API token @BotFather
             events=[Events.STREAMER_ONLINE, Events.STREAMER_OFFLINE,
                     Events.BET_LOSE],                                               # Only these events will be sent to the chat
             disable_notification=True,                                              # Revoke the notification (sound/vibration)
